5c448d19511b393e68ff5939cfc68b4561a1ce82
l6828833-ship-it <l6828833@gmail.com>
2026-04-05 17:47:26 +0100

    Delete .pnpmfile.cjs

--- .pnpmfile.cjs
+++
@@ -1,9 +0,0 @@
-// Approve build scripts for packages that need them on Railway
-// This fixes the "Ignored build scripts: @tailwindcss/oxide, esbuild" warning
-module.exports = {
-  hooks: {
-    readPackage(pkg) {
-      return pkg;
-    },
-  },
-};
